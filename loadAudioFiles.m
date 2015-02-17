20b8199d6b1d0fefbc680f8906e0158e8b530b25
lorenzo sernicola <lorenzo971@gmail.com>
2015-02-17 17:02:55 +0100

    "corrections"

--- loadAudioFiles.m
+++ loadAudioFiles.m
@@ -1,30 +1,12 @@
-function icaInput = loadAudioFiles(folder)
-out = [];
-dims = [];
+function dataSet = loadAudioFiles(folder)
+% Load all the audio files contained in a specified folder
 d=dir([folder '/*.wav']);
 
-for ii=1:length(d)
-    fname=d(ii).name;
-    y =audioread(fname);
-    dims = [dims size(y,1)];
-end
-
-
-media = floor(mean(dims))
-if media > 440000
-    media = 440000;
-end
 
 for ii=1:length(d)
     fname=d(ii).name;
-    y = audioread(fname);
-    f = sum(y,2);
-    if (length(f) > media)
-        out = [out f(1:media)];
-    else
-        f(media) = 0;
-        out = [out f];
-    end
+    [sig , freq] = audioread(fname);
+    dataSet(ii).sig = sum(sig,2);
+    dataSet(ii).freq = freq;
 end
-icaInput = out;
 
